71ebaf124e7f3957e5e411825514d72efab51ed0
Ignacio <ignacionicolaslaureanogarcia@a.cifpperedesongall.org>
2024-06-06 19:04:09 +0200

    Added some of the functions of the player menu

--- src/Menu.java
+++ src/Menu.java
@@ -1,4 +1,5 @@
 import java.sql.*;
+import java.util.InputMismatchException;
 
 public class Menu {
 
@@ -349,9 +350,165 @@ public class Menu {
         System.out.println("You logged in as Community Moderator");
     }
 
-    public static void Player(Connection connection) {
+    public static void Player(Connection connection) throws SQLException {
+        boolean exit = false;
+        Statement statement = connection.createStatement();
+        statement.execute("USE chaos_conquest");
 
         System.out.println("You logged in as a Player");
+        do {
+            System.out.println("Select the table you want to use or put 0 to close the actual session:");
+            System.out.println("0.Close session");
+            System.out.println("1.Add or see an existing ItemLoadout");
+            System.out.println("2.Add or see an existing RuneLoadout");
+            System.out.println("3. Check players on the server");
+            System.out.println("4.See all Champions");
+            System.out.println("5.Check the abilities of a Champion");
+            System.out.println("6.Check the skins of a Champion");
+            System.out.println("7.Check Clashes");
+            System.out.println("8.Check Clash Participants");
+            System.out.println("9.See Competitive Ranks");
+            System.out.println("10.See Damage Types");
+            System.out.println("11.Check Items");
+            System.out.println("12.Check ItemLoadout");
+            System.out.println("13.Check ItemStats");
+            System.out.println("14.Check Mastery");
+            System.out.println("15.Check MatchHistory");
+            System.out.println("16.Check MatchStats");
+            System.out.println("17.Check OwnedChampion");
+            System.out.println("19.Check PatchHistory");
+            System.out.println("21.Check Runes");
+            System.out.println("22.Check Rune Loadouts");
+            System.out.println("23.Check Rune Paths");
+            System.out.println();
+
+            int option = Lectura.numeroEnter();
+
+            ResultSet resultSet;
+            int columnCount;
+
+            switch (option) {
+
+                case 0:
+
+                    exit = true;
+                    break;
+
+                case 1:
+                    try{
+                        System.out.println("Insert the number of the ItemID:");
+                        int newItemID = Lectura.numeroEnter();
+
+                        statement.executeUpdate("INSERT INTO itemloadout(ItemID) VALUES (" + newItemID + ")");
+                        System.out.println("Inserting item loadout...");
+                        System.out.println("Done");
+                        break;
+                    } catch (InputMismatchException e){
+                        System.out.println("Error: could not insert the itemloadout, please insert the ItemID (number)");
+                        break;
+                    }
+
+                case 2:
+                    try{
+                        System.out.println("Insert the number of the RuneID:");
+                        int newRuneID = Lectura.numeroEnter();
+
+                        statement.executeUpdate("INSERT INTO runeloadout(ItemID) VALUES (" + newRuneID + ")");
+                        System.out.println("Inserting rune loadout...");
+                        System.out.println("Done");
+                        break;
+                    } catch (InputMismatchException e) {
+                        System.out.println("Error: could not insert the runeloadout, please insert the ItemID (number)");
+                        break;
+                    }
+
+                case 3:
+                    resultSet = statement.executeQuery("SELECT * FROM player");
+                    columnCount = resultSet.getMetaData().getColumnCount();
+
+                    while (resultSet.next()) {
+                        for (int i = 1; i <= columnCount; i++) {
+                            System.out.print(resultSet.getString(i) + "\t");
+                        }
+                        System.out.println();
+                    }
+                    break;
+
+                case 4:
+
+                    resultSet = statement.executeQuery("SELECT * FROM champion");
+                    columnCount = resultSet.getMetaData().getColumnCount();
+
+                    while (resultSet.next()) {
+                        for (int i = 1; i <= columnCount; i++) {
+                            System.out.print(resultSet.getString(i) + "\t");
+                        }
+                        System.out.println();
+                    }
+                    break;
+
+                case 5:
+                    System.out.println("Introduce the ID of the champion you want to see the abilities of:");
+                    int abilityChampionID = Lectura.numeroEnter();
+
+                    resultSet = statement.executeQuery("SELECT * from championabilities WHERE championID = " + abilityChampionID);
+                    columnCount = resultSet.getMetaData().getColumnCount();
+
+                    while (resultSet.next()) {
+                        for (int i = 1; i <= columnCount; i++) {
+                            System.out.println(resultSet.getString(i) + "\t");
+                        }
+                        System.out.println();
+                    }
+                    break;
+
+                case 6:
+                    System.out.println("Introduce the ID of the champion you want to see the skins of:");
+                    int skinChampionID = Lectura.numeroEnter();
+
+                    resultSet = statement.executeQuery("SELECT * from championabilities WHERE championID = " + skinChampionID);
+                    columnCount = resultSet.getMetaData().getColumnCount();
+
+                    while (resultSet.next()) {
+                        for (int i = 1; i <= columnCount; i++) {
+                            System.out.println(resultSet.getString(i) + "\t");
+                        }
+                        System.out.println();
+                    }
+                    break;
+
+                case 7:
+                    resultSet = statement.executeQuery("SELECT * from clash");
+                    columnCount = resultSet.getMetaData().getColumnCount();
+
+                    while (resultSet.next()) {
+                        for (int i = 1; i <= columnCount; i++) {
+                            System.out.println(resultSet.getString(i) + "\t");
+                        }
+                        System.out.println();
+                    }
+                    break;
+
+                case 8:
+                    System.out.println("Introduce the ID of the clash from who you want to see the participants of:");
+                    int participantsClashID = Lectura.numeroEnter();
+
+                    resultSet = statement.executeQuery("SELECT * FROM clashparticipant WHERE ClashID = " + participantsClashID);
+                    columnCount = resultSet.getMetaData().getColumnCount();
+
+                    while (resultSet.next()) {
+                        for (int i = 1; i <= columnCount; i++) {
+                            System.out.println(resultSet.getString(i) + "\t");
+                        }
+                        System.out.println();
+                    }
+                    break;
+                    default:
+
+                    System.out.println("Invalid option");
+                    break;
+            }
+        } while (!exit);
     }
 
     public static void PublicVisitant(Connection connection) {
